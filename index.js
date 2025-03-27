246500e8f0e031a4d37ac489aa7ad71445d6316a
Antony Omondi <tonymboya13@gmail.com>
2025-03-27 21:28:00 +0800

    complete array filter method

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution here
+
+driverNames = ["Tony", "Mboya", "Omondi", "tony"]
+
+// function findMatching(drivers, name){
+//     let newNameArray = [];
+
+//     for(const driver of drivers){
+//         if(driver.toLowerCase() === name.toLowerCase()){
+//             newNameArray.push(driver);
+//         }
+//     }
+//     return newNameArray;
+// }
+// findMatching(names, "tony")
+
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+findMatching(driverNames, "tony")
+
+function fuzzyMatch(drivers, char){
+    return drivers.filter(driver => driver.toLowerCase().startsWith(char.toLowerCase()));
+}
+
+function matchName(drivers, name){
+    return drivers.filter(driver => driver.name === name);  
+}
