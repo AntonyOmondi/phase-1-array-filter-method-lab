862cd0a93dba82c4182f8edc5c11f3f324ec1069
Antony Omondi <tonymboya13@gmail.com>
2025-03-27 21:29:52 +0800

    Complete array filter methods lab

--- index.js
+++ index.js
@@ -2,17 +2,6 @@
 
 driverNames = ["Tony", "Mboya", "Omondi", "tony"]
 
-// function findMatching(drivers, name){
-//     let newNameArray = [];
-
-//     for(const driver of drivers){
-//         if(driver.toLowerCase() === name.toLowerCase()){
-//             newNameArray.push(driver);
-//         }
-//     }
-//     return newNameArray;
-// }
-// findMatching(names, "tony")
 
 function findMatching(drivers, name) {
     return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
@@ -22,7 +11,9 @@ findMatching(driverNames, "tony")
 function fuzzyMatch(drivers, char){
     return drivers.filter(driver => driver.toLowerCase().startsWith(char.toLowerCase()));
 }
+fuzzyMatch(driverNames, "T")
 
 function matchName(drivers, name){
     return drivers.filter(driver => driver.name === name);  
 }
+matchName(driverNames, "Omondi")
